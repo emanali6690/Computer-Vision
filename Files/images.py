34e3f61842650e6ebed1a57ad9af8aea1ed47040
emanali6690 <eali666690@gmail.com>
2024-09-15 23:13:55 +0500

    Update images.py

--- Files/images.py
+++ Files/images.py
@@ -1,1 +1,12 @@
+import cv2
+
+# Load an image from the Images folder
+img = cv2.imread('Images/sample_image.jpg')
+
+# Display the image in a window
+cv2.imshow('Image', img)
+
+# Wait until a key is pressed to close the window
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
